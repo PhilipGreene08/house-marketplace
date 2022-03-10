718045266187362a67b3ab7b3dcda44158af22ef
Philip Grene <54162361+PhilipGreene08@users.noreply.github.com>
2022-03-09 21:01:59 -0600

    Add files via upload

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -14,6 +14,7 @@ import { db } from '../firebase.config';
 import { toast } from 'react-toastify';
 import Spinner from '../components/Spinner';
 import { Fragment } from 'react';
+import ListingItem from '../components/ListingItem';
 
 function Category() {
   const [listings, setListings] = useState(null);
@@ -36,6 +37,7 @@ function Category() {
         const querySnap = await getDocs(q);
         let listings = [];
         querySnap.forEach((doc) => {
+          console.log(doc.id, doc.data());
           return listings.push({ id: doc.id, data: doc.data() });
         });
 
@@ -67,7 +69,11 @@ function Category() {
           <main>
             <ul className='categoryListings'>
               {listings.map((listing) => (
-                <h3 key={listing.id}>{listing.data.name}</h3>
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
               ))}
             </ul>
           </main>
